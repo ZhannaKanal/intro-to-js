9248aac305c5a4f229769f27de0c5d1893119a1c
Zhanna <kezhanna.03@mail.ru>
2025-10-25 22:01:04 +0500

    76-77 tasks commenting

--- script.js
+++ script.js
@@ -101,9 +101,10 @@ function newPadRow(name) {
 function secondPadRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-for (let i = 1; i <= count; i++) {
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
-}
+}*/
 result = ""
 
 for (const row of rows) {
