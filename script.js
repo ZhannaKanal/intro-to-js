b26f03db4a75a1cc0114f1ef5bbff047dd4eec73
Zhanna <kezhanna.03@mail.ru>
2025-10-26 23:50:07 +0500

    101's task

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ let done = 0;
 //   rows.push(padRow(rows.length + 1, count));
 // }
 //98 task is about commenting while loop
-//99-100
-for (let i = count; i > 0; false) {
+//99-100-101
+for (let i = count; i > 0; i = i - 1) {
 
 }
